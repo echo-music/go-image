412a475d09bd984cda5c1eb66da7f192ab2835b4
echo music <php_fangting@126.com>
2024-06-05 21:36:16 +0800

    图片合成

---
+++ marker/cmd/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"go-image/marker"
+	"image"
+	"image/draw"
+
+	"github.com/disintegration/imaging"
+)
+
+func main() {
+	var imgObj = marker.NewImage()
+	bgImg, err := imgObj.Background("../images/bg.png")
+	if err != nil {
+		panic(err)
+	}
+
+	// 头像设置
+	petRoleImg, err := imgObj.Circle("../images/dog.png", 180)
+	if err != nil {
+		panic(err)
+	}
+	userImg, err := imgObj.Circle("../images/girl.png", 80)
+	if err != nil {
+		panic(err)
+	}
+
+	// 坐标设置
+	draw.Draw(bgImg, petRoleImg.Bounds().Add(image.Pt(18, 15)), petRoleImg, image.Point{}, draw.Over)
+	draw.Draw(bgImg, userImg.Bounds().Add(image.Pt(3, 123)), userImg, image.Point{}, draw.Over)
+
+	// 保存到临时文件
+	filepath := "../images/marker.png"
+	err = imaging.Save(bgImg, filepath)
+	if err != nil {
+		panic(err)
+	}
+
+}
